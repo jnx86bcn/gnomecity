89aecb22ddfd75522df5b7acd02ec2f52bc5d8a1
Juan Gasparino <juan.gasparino@birchmangroup.com>
2020-01-29 17:32:58 +0100

    update call function getGnomes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SearchBar, Filters } from '../Search';
 import { Dashboard } from '../Dashboard';
 import { Spinner } from '../Spinner';
+import { useDispatch } from 'react-redux';
+import { actions_setItems,actions_setLoading } from '../../redux/actions/actions';
+import { Gnome } from '../../models';
+import { getAllItems_DEV } from '../../services';
 
 export function App(): JSX.Element {
+
+  useEffect(()=>getGnomes(),[])
+
+  const dispatch = useDispatch();
+
+  function getGnomes() {
+    dispatch(actions_setLoading(true));
+    getAllItems_DEV().
+    then((items: Gnome[]) => {
+      dispatch(actions_setItems(items));
+      dispatch(actions_setLoading(false));
+    })
+  }
 
   return (
     <>
--- src/components/Search/SearchBar.tsx
+++ src/components/Search/SearchBar.tsx
@@ -1,8 +1,6 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { getAllItems_DEV } from '../../services';
-import { Gnome } from '../../models';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { actions_setItems,actions_setLoading,actions_setShowFilters } from '../../redux/actions/actions';
+import { actions_setShowFilters } from '../../redux/actions/actions';
 
 
 export function SearchBar(): JSX.Element {
@@ -11,25 +9,10 @@ export function SearchBar(): JSX.Element {
 
   const showFilter = useSelector( (state:any) => state.global.showFilter );
 
-  useEffect(() => {
-    getGnomes();
-  },[])
-
-  function getGnomes() {
-    dispatch(actions_setItems([]))
-    dispatch(actions_setLoading(true));
-    getAllItems_DEV().
-    then((items: Gnome[]) => {
-      dispatch(actions_setItems(items));
-      dispatch(actions_setLoading(false));
-    })
-  }
-
 
   function updateShowFilter() {
     showFilter == false ? dispatch(actions_setShowFilters(true)) : dispatch(actions_setShowFilters(false))
   }
-
 
   return (
     <>
